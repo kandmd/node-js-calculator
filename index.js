27c185de7b55483247cd03b477b1f8fef842bf4f
Ivan Eisenberg <ivaneisenberg@northwesternmutual.com>
2017-03-11 15:15:25 -0600

    Added multiply, fixed missing semi-colon :)

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ function subtract (num1, num2) {
 	return num1 - num2;
 }
 
+function multiply (num1, num2) {
+	return num1 * num2;
+}
+
 add(1, 1); // returns 2
-subtract(4, 2) // returns 2
+subtract(4, 2); // returns 2
+multiply(2, 2); // returns 4
